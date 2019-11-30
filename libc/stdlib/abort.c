d33ec3444a1ac1c8ce269f4176ae922fa2f38056
Ruby Lord <nipkraps@gmail.com>
2019-11-30 14:54:52 -0600

    stdlib: use panic for abort

--- libc/stdlib/abort.c
+++ libc/stdlib/abort.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <kernel/panic.h>
 
 __attribute__((__noreturn__))
 void abort() {
 #if defined(__is_libk)
-	// TODO: Add proper kernel panic.
-	printf("kernel: panic: abort()\n");
+	panic("abort()");
 #else
 	// TODO: Abnormally terminate the process as if by SIGABRT.
 	printf("abort()\n");
