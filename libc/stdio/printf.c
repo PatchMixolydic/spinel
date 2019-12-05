34371edafb35f14eb18b415ef37382acc43e3e85
Ruby Lord <nipkraps@gmail.com>
2019-12-04 18:50:58 -0600

    stdio: further changes to printf, swap out if statement for switch

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -6,9 +6,11 @@
 
 static bool print(const char* data, size_t length) {
 	const unsigned char* bytes = (const unsigned char*) data;
-	for (size_t i = 0; i < length; i++)
-		if (putchar(bytes[i]) == EOF)
+	for (size_t i = 0; i < length; i++) {
+		if (putchar(bytes[i]) == EOF) {
 			return false;
+		}
+	}
 	return true;
 }
 
@@ -19,59 +21,74 @@ int printf(const char* restrict format, ...) {
 	int written = 0;
 
 	while (*format != '\0') {
-		size_t maxrem = INT_MAX - written;
+		size_t maxRemaining = INT_MAX - written;
 
 		if (format[0] != '%' || format[1] == '%') {
-			if (format[0] == '%')
+			if (format[0] == '%') {
 				format++;
+			}
 			size_t amount = 1;
-			while (format[amount] && format[amount] != '%')
+			while (format[amount] && format[amount] != '%') {
 				amount++;
-			if (maxrem < amount) {
+			}
+			if (maxRemaining < amount) {
 				// TODO: Set errno to EOVERFLOW.
 				return -1;
 			}
-			if (!print(format, amount))
+			if (!print(format, amount)) {
 				return -1;
+			}
 			format += amount;
 			written += amount;
 			continue;
 		}
 
-		const char* format_begun_at = format++;
+		const char* originalFormat = format++;
 
-		if (*format == 'c') {
-			format++;
-			char c = (char) va_arg(parameters, int /* char promotes to int */);
-			if (!maxrem) {
-				// TODO: Set errno to EOVERFLOW.
-				return -1;
+		switch (*format) {
+			case 'c': {
+				format++;
+				char c = (char) va_arg(parameters, int); // char promotes to int
+				if (!maxRemaining) {
+					// TODO: Set errno to EOVERFLOW.
+					return -1;
+				}
+				if (!print(&c, sizeof(c))) {
+					return -1;
+				}
+				written++;
+				break;
 			}
-			if (!print(&c, sizeof(c)))
-				return -1;
-			written++;
-		} else if (*format == 's') {
-			format++;
-			const char* str = va_arg(parameters, const char*);
-			size_t len = strlen(str);
-			if (maxrem < len) {
-				// TODO: Set errno to EOVERFLOW.
-				return -1;
+
+			case 's': {
+				format++;
+				const char* str = va_arg(parameters, const char*);
+				size_t len = strlen(str);
+				if (maxRemaining < len) {
+					// TODO: Set errno to EOVERFLOW.
+					return -1;
+				}
+				if (!print(str, len)) {
+					return -1;
+				}
+				written += len;
+				break;
 			}
-			if (!print(str, len))
-				return -1;
-			written += len;
-		} else {
-			format = format_begun_at;
-			size_t len = strlen(format);
-			if (maxrem < len) {
-				// TODO: Set errno to EOVERFLOW.
-				return -1;
+
+			default: {
+				format = originalFormat;
+				size_t len = strlen(format);
+				if (maxRemaining < len) {
+					// TODO: Set errno to EOVERFLOW.
+					return -1;
+				}
+				if (!print(format, len)) {
+					return -1;
+				}
+				written += len;
+				format += len;
+				break;
 			}
-			if (!print(format, len))
-				return -1;
-			written += len;
-			format += len;
 		}
 	}
 
