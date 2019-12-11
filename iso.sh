b211c637be67a445467c2bf5fb447fb6788d7920
Ruby Lord <nipkraps@gmail.com>
2019-12-10 23:36:21 -0600

    build: kernel is now spinel.elf, prepare a bit for multiarch

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/spinel.kernel isodir/boot/spinel.kernel
+cp sysroot/boot/spinel.elf isodir/boot/spinel.elf
 cat > isodir/boot/grub/grub.cfg << EOF
 menuentry "spinel" {
-	multiboot /boot/spinel.kernel
+	multiboot /boot/spinel.elf
 }
 EOF
 grub-mkrescue -o spinel.iso isodir
--- target-triplet-to-arch.sh
+++ target-triplet-to-arch.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 if echo "$1" | grep -Eq 'i[[:digit:]]86-'; then
   echo i386
+elif echo "$1" | grep -Eq 'x86_64-'; then
+  echo amd64
 else
   echo "$1" | grep -Eo '^[[:alnum:]_]*'
 fi
