a50266f2b4df4be37f63c0d9f57f610563059abb
Ruby Lord <nipkraps@gmail.com>
2020-02-25 19:41:15 -0600

    i386: i swear i know how stacks work
    i need a vacation; not from this, just from school so i can actually
    focus on this

--- src/kernel/arch/i386/core/cpu.asm
+++ src/kernel/arch/i386/core/cpu.asm
@@ -1,5 +1,11 @@
 section .text
     global haltCPU:function
+    global inByte:function
+    global outByte:function
+    global inWord:function
+    global outWord:function
+    global inDWord:function
+    global outDWord:function
     global loadGDT:function
     global loadIDT:function
     global getESP:function
@@ -8,6 +14,39 @@ section .text
 
     haltCPU:
         hlt
+        ret
+
+    inByte:
+        mov     dx, [esp + 4] ; address
+        in      al, dx
+        ret
+
+    outByte:
+        mov     al, [esp + 8] ; data
+        mov     dx, [esp + 4] ; address
+        out     dx, al
+        ret
+
+    inWord:
+        mov     dx, [esp + 4] ; address
+        in      ax, dx
+        ret
+
+    outWord:
+        mov     ax, [esp + 8] ; data
+        mov     dx, [esp + 4] ; address
+        out     dx, ax
+        ret
+
+    inDWord:
+        mov     dx, [esp + 4] ; address
+        in      eax, dx
+        ret
+
+    outDWord:
+        mov     eax, [esp + 8] ; data
+        mov     dx, [esp + 4] ; address
+        out     dx, eax
         ret
 
     loadGDT:
--- src/kernel/arch/i386/core/cpu.h
+++ src/kernel/arch/i386/core/cpu.h
@@ -4,41 +4,16 @@
 #include <stdint.h>
 #include <spinel/cpu.h>
 
+uint8_t inByte(uint16_t port);
+void outByte(uint16_t port, uint8_t data);
+uint16_t inWord(uint16_t port);
+void outWord(uint16_t port, uint16_t data);
+uint32_t inDWord(uint16_t port);
+void outDWord(uint16_t port, uint32_t data);
+
 void loadGDT(uintptr_t loc);
 void loadIDT(uintptr_t loc);
 
 uintptr_t getESP(void);
-
-// I can't get these to work in NASM
-
-static inline uint8_t inByte(uint16_t port) {
-    uint8_t res;
-    __asm volatile ("inb %1, %0" : "=a"(res) : "Nd"(port));
-    return res;
-}
-
-static inline void outByte(uint16_t port, uint8_t val) {
-    __asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
-}
-
-static inline uint16_t inWord(uint16_t port) {
-    uint16_t res;
-    __asm volatile ("inw %1, %0" : "=a"(res) : "Nd"(port));
-    return res;
-}
-
-static inline void outWord(uint16_t port, uint16_t val) {
-    __asm volatile ("outw %0, %1" : : "a"(val), "Nd"(port));
-}
-
-static inline uint32_t inDWord(uint16_t port) {
-    uint32_t res;
-    __asm volatile ("ind %1, %0" : "=a"(res) : "Nd"(port));
-    return res;
-}
-
-static inline void outDWord(uint16_t port, uint32_t val) {
-    __asm volatile ("outd %0, %1" : : "a"(val), "Nd"(port));
-}
 
 #endif // ndef CPU_H
