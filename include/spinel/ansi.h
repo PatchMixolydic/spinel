62b702c04a9bc75e0f825a2c70104b7f5fd5045f
Ruby Lord <nipkraps@gmail.com>
2020-02-25 20:33:39 -0600

    ansi: missed a "const"

--- include/spinel/ansi.h
+++ include/spinel/ansi.h
@@ -3,6 +3,6 @@
 
 // Takes a string starting with an ANSI escape and parses it.
 // Returns the number of characters to skip to skip the escape code.
-int parseANSIEscape(char s[]);
+int parseANSIEscape(const char s[]);
 
 #endif // ndef SPINEL_ANSI_H
