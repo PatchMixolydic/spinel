774e46238f8ba33ef529028bea97540e307046ea
Ruby Lord <nipkraps@gmail.com>
2020-04-17 23:44:45 -0500

    build: use -serial stdio for QEMU instead of -serial mon:stdio
    This allows for QEMU to be closed using ctrl+C

--- build.sh
+++ build.sh
@@ -94,7 +94,7 @@ qemu() {
         iso
     fi
     qemu-system-$(./tripletToArch.sh $HOST) -cdrom build/spinel.iso -s -m 512 \
-        -soundhw pcspk -enable-kvm -serial mon:stdio
+        -soundhw pcspk -enable-kvm -serial stdio
 }
 
 rotate() {
