1018ab9504d132b88103a10f7cb5de2f642ed8f1
Ruby Lord <nipkraps@gmail.com>
2020-04-23 01:46:19 -0500

    timer: Add alarms
    You can set an alarm any time after kmalloc is initialized.
    These allow a callback to be called after a given delay. One-shot
    alarms are also available.

--- src/kernel/devices/timer.c
+++ src/kernel/devices/timer.c
@@ -1,16 +1,29 @@
+#include <assert.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <dastr/linkedList.h>
 #include <spinel/timer.h>
 
 // TODO: should there be some sort of timer registration system?
 // It seems better to just let one timer handle everything
 
+typedef struct {
+    uint64_t id;
+    uint64_t interval;
+    uint64_t ticks;
+    bool oneShot;
+    TimerAlarmCallback callback;
+} Alarm;
+
 // Ticks are in hundredths of a nanosecond
+const uint64_t TicksPerSecond = 100000000000;
 
-static const uint64_t TicksPerSecond = 100000000000;
-
+static uint64_t nextTimerId = 1;
 static uint64_t ticksSinceBoot = 0;
 static uint64_t ticksPerCycle = 0;
+static LinkedList* alarms = NULL;
 
 // Convert a given frequency to ticks per cycle
 // Note: this doesn't work if the clock period is less than 1/100 ns
@@ -22,13 +35,70 @@ void timerSetFrequency(uint64_t newFrequency) {
     ticksPerCycle = frequencyToTicks(newFrequency);
 }
 
+uint64_t timerSetAlarm(
+    uint64_t interval, bool oneShot, TimerAlarmCallback callback
+) {
+    if (alarms == NULL) {
+        alarms = linkedListCreate();
+    }
+    assert(callback);
+
+    Alarm* newAlarm = malloc(sizeof(Alarm));
+    // TODO: think of a better way to handle ids
+    if (nextTimerId == 0) {
+        nextTimerId++;
+    }
+    newAlarm->id = nextTimerId++;
+    newAlarm->interval = interval;
+    newAlarm->oneShot = oneShot;
+    newAlarm->callback = callback;
+    newAlarm->ticks = 0;
+    linkedListInsertLast(alarms, newAlarm);
+    return newAlarm->id;
+}
+
+void timerRemoveAlarm(uint64_t id) {
+    if (alarms == NULL) {
+        printf("Tried to remove alarm when none were registered\n");
+        return;
+    }
+
+    ForEachInList(alarms, alarmNode) {
+        Alarm* alarm = alarmNode->data;
+        assert(alarm);
+        if (id != alarm->id) {
+            continue;
+        }
+
+        linkedListRemoveNode(alarms, alarmNode);
+        return;
+    }
+
+    printf("Tried to remove timer %u, but it wasn't registered!\n", id);
+}
+
 void timerTick(void) {
     ticksSinceBoot += ticksPerCycle;
     if (ticksSinceBoot == 0) {
         printf("ticksSinceBoot overflowed\n");
     }
+
+    if (alarms != NULL) {
+        ForEachInList(alarms, alarmNode) {
+            Alarm* alarm = alarmNode->data;
+            assert(alarm);
+            alarm->ticks += ticksPerCycle;
+            if (alarm->ticks >= alarm->interval) {
+                alarm->callback();
+                alarm->ticks = 0;
+                if (alarm->oneShot) {
+                    timerRemoveAlarm(alarm->id);
+                }
+            }
+        }
+    }
 }
 
-uint64_t timerGetTicks(void) {
+uint64_t timerGetTicksSinceBoot(void) {
     return ticksSinceBoot;
 }
--- src/kernel/include/spinel/timer.h
+++ src/kernel/include/spinel/timer.h
@@ -1,10 +1,23 @@
 #ifndef SPINEL_TIMER_H
 #define SPINEL_TIMER_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
+extern const uint64_t TicksPerSecond;
+
+typedef void (*TimerAlarmCallback)(void);
+
 void timerSetFrequency(uint64_t newFrequency);
+// Set an alarm to go off. After `time` ticks, `callback` will be
+// called. If `oneShot` is set, the alarm will only go off once.
+// Othewise, it will go off every `time` ticks.
+// Returns the timer ID, which can be used to remove the alarm.
+uint64_t timerSetAlarm(
+    uint64_t interval, bool oneShot, TimerAlarmCallback callback
+);
+void timerRemoveAlarm(uint64_t id);
 void timerTick(void);
-uint64_t timerGetTicks(void);
+uint64_t timerGetTicksSinceBoot(void);
 
 #endif // ndef SPINEL_TIMER_H
