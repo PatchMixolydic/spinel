5faa00a861814ec1a32bd0cc50cffb6526e71fec
Ruby Lord <nipkraps@gmail.com>
2020-04-29 21:22:25 -0500

    libc: Check for overflow when multiplying in calloc
    Note that this only supports compilers which claim to be GCC compatible
    (ex. GCC and Clang). When compiled with other compilers, libc will fall
    back on just multiplying the two together without checking for
    overflow.

--- src/lib/libc/stdlib/calloc.c
+++ src/lib/libc/stdlib/calloc.c
@@ -1,7 +1,36 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-// TODO: This is wrong! This should avoid overflow
+#ifdef __Kernel
+#include <spinel/panic.h>
+#else
+#include <stdio.h>
+#endif
+
 void* calloc(size_t nmemb, size_t size) {
-    return malloc(nmemb * size);
+    size_t mallocSize;
+    #ifdef __GNUC__
+        // Use a GCC builtin to check for overflow
+        if (!__builtin_mul_overflow(nmemb, size, &mallocSize)) {
+            #ifdef __Kernel
+                panic(
+                    "calloc multiplication overflowed: "
+                    "%u elements of size %u\n",
+                    nmemb, size
+                );
+            #else
+                // TODO: stderr
+                printf(
+                    "calloc multiplication overflowed: "
+                    "%u elements of size %u\n",
+                    nmemb, size
+                );
+                // TODO: abort
+            #endif
+        }
+    #else
+        // TODO: is there a way to check for overflow here?
+        mallocSize = nmemb * size;
+    #endif
+    return malloc(mallocSize);
 }
