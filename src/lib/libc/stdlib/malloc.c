ee69781da29658be50687ebdd6a39ac3f089110e
Ruby Lord <nipkraps@gmail.com>
2020-05-28 17:22:54 -0500

    libc: Simplify malloc
    Back to a free list... at least this design's a little less prone to
    issues. There are a couple of bugs and a hackfix, unfortunately, but
    I can't exactly find them right now. They appear to require a bunch
    of sequential large allocations. The nature of these bugs ranges from
    a block's previous block's next block (ie. the original block) not
    being equal to the original block (this was hackfixed) and the usual
    weird disable-paging-and-triple-fault (maybe not malloc's fault?).

--- src/lib/libc/stdlib/malloc.c
+++ src/lib/libc/stdlib/malloc.c
@@ -13,158 +13,97 @@
 #include <spinel/virtualMemory.h>
 #endif
 
-// Simple memory allocator
-// Uses buddy allocation up to 4KiB, then mmap
-// Tries to stay 4-byte aligned for compatibility
-// with processors that need it
+// Simpler memory allocator
 
-// Cool fact: Spinel's mmap will, when given NULL for the address,
-// place the memory mapping at the current heap's end
-// So we don't need sbrk, mmap will do all the work
-
-// Since our allocations should be 8-byte aligned
-// and we use four bytes for metadata, a minimum order of 4
-// (= 16 bytes) makes sense
-#define BuddyMinOrder 4
-// Maximum order of 12 = 4 KiB
-// After this point, we will just mmap in the result
-#define BuddyMaxOrder 12
-// Size of the block pool to allocate at one time
-// If we don't have enough blocks to meet a request,
+// Size of the pool to allocate at one time
+// If we don't have enough free space to meet a request,
 // we will allocate another pool
 #define PoolSize (16 * KiB)
-// Number of max order blocks in a pool
-// pow2 is an inline function defined below
-#define BlocksInPool (PoolSize / pow2(BuddyMaxOrder))
+#define MinBlockSize (16 + sizeof(MemoryBlock)) // bytes
+#define Min(x,y) ((x) < (y) ? (x) : (y))
+#define Max(x,y) ((x) > (y) ? (x) : (y))
 
-typedef struct FreeMemoryBlock {
-    uint8_t order;
-    uint8_t isFree;
-    uint8_t padding[2];
-    struct FreeMemoryBlock* next;
+typedef struct MemoryBlock {
+    uint64_t size : 63;
+    uint64_t isFree : 1;
+    struct MemoryBlock* nextFree;
+    struct MemoryBlock* nextBlock;
+    struct MemoryBlock* prevBlock;
     uint8_t data[];
-} __attribute__((packed)) FreeMemoryBlock;
+} __attribute__((packed)) MemoryBlock;
 
-typedef struct AllocMemoryBlock {
-    uint8_t order;
-    uint8_t isFree;
-    uint8_t padding[6];
-    uint8_t data[];
-} __attribute__((packed)) AllocMemoryBlock;
-
-static FreeMemoryBlock* freeLists[BuddyMaxOrder - BuddyMinOrder + 1];
+static MemoryBlock* freeList = NULL;
+static MemoryBlock* largestBlockAddress = NULL;
 
 #ifdef __Kernel
 static void* heapEnd = (void*)KernelHeapStart;
 #endif
 
-// Integer log2
-static inline size_t lg(size_t x) {
-    size_t res = 0;
-    size_t val = x;
-    while (val > 1) {
-        val >>= 1;
-        res++;
-    }
-    return res;
+static inline void blockSetSize(MemoryBlock* block, uint64_t size) {
+    assert(block != NULL);
+    block->size = (size & ~1) >> 1;
 }
 
-// Get a power of 2
-static inline size_t pow2(size_t exp) {
-    return 1 << exp;
+static inline uint64_t blockGetSize(MemoryBlock* block) {
+    assert(block != NULL);
+    return block->size << 1;
 }
 
-static inline size_t roundToPowerOf2(size_t x) {
-    // 1 is subtracted from x in case x is already a power of 2,
-    // so that the function will effectively return x
-    return pow2(lg(x - 1) + 1);
+// Literal next block, not next free block
+static inline MemoryBlock* blockGetNextBlock(MemoryBlock* block) {
+    assert(block != NULL);
+    return (MemoryBlock*)((uintptr_t)block + (uintptr_t)blockGetSize(block));
 }
 
-static inline FreeMemoryBlock* getBuddy(void* addr, size_t size) {
-    return (FreeMemoryBlock*)((uintptr_t)addr ^ size);
+static inline void blockMergeWithNext(MemoryBlock* block) {
+        assert(block != NULL);
+        assert(block->nextBlock != NULL && block->nextBlock->isFree);
+
+        size_t newSize = blockGetSize(block) + blockGetSize(block->nextBlock);
+        block->nextBlock = block->nextBlock->nextBlock;
+        if (block->nextBlock) {
+            block->nextBlock->prevBlock = block;
+        }
+        blockSetSize(block, newSize);
+}
+
+static inline void freeListAdd(MemoryBlock* block) {
+    assert(block != NULL);
+    assert(block->isFree);
+    block->nextFree = freeList;
+    freeList = block;
 }
 
 static inline void* alignPtr(void* addr) {
     return (void*)(((uintptr_t)addr + 7) & ~7);
 }
 
-static inline void* voidToMemoryBlock(void* addr) {
-    return (void*)((uintptr_t)addr - 8);
+static inline MemoryBlock* voidToMemoryBlock(void* addr) {
+    return (MemoryBlock*)((uintptr_t)addr - sizeof(MemoryBlock));
 }
 
 static inline size_t memSizeToBlockSize(size_t size) {
-    return size + 8;
-}
-
-#ifdef __Kernel
-// The kernel doesn't get to have syscalls, so it needs shims
-static inline void* mmap(
-    void* addr, size_t length, int prot, int flags, int fd, off_t offset
-) {
-    // We can make some reasonable assumptions:
-    // addr is NULL, we're trying to map at the end of the heap (program break)
-    // prot = PROT_READ | PROT_WRITE
-    // flags = MAP_ANONYMOUS
-    // and thus fd is -1 and offset is 0
-    (void)prot;
-    (void)flags;
-    (void)fd;
-    (void)offset;
-
-    if (length == 0) {
-        errno = EINVAL;
-        return MAP_FAILED;
+    size_t res = size + sizeof(MemoryBlock);
+    // Must be even for 63-bit size value
+    if (res % 2 == 1) {
+        ++res;
     }
-
-    void* newEnd = (void*)((uintptr_t)heapEnd + length);
-    newEnd = alignPtr(newEnd);
-    if (newEnd >= (void*)KernelHeapEnd) {
-        panic(
-            "Out of heap: tried to extend heap from 0x%X to 0x%X",
-            heapEnd, newEnd
-        );
-    } else if (newEnd < (void*)KernelHeapStart) {
-        // !?!?!
-        panic(
-            "Out of heap: heap overflowed from 0x%X to 0x%X",
-            heapEnd, newEnd
-        );
-    }
-
-    int ret = vMemMapInRange(heapEnd, newEnd, VMemRead | VMemWrite);
-    if (ret < 0) {
-        panic("vMemMapInRange failed: %s", strerror(-ret));
-    }
-
-    void* res = heapEnd;
-    heapEnd = newEnd;
     return res;
 }
 
-static inline int munmap(void* addr, size_t length) {
-    if (length == 0) {
-        errno = EINVAL;
-        return -1;
-    }
-
-    void* end = (void*)((uintptr_t)addr + length);
-    end = alignPtr(end);
-    if (
-        addr < (void*)KernelHeapStart || addr >= (void*)KernelHeapEnd
-    ) {
-        panic("Tried to shim munmap starting at invalid address 0x%X", addr);
-    } else if (end < (void*)KernelHeapStart || end > heapEnd) {
+#ifdef __Kernel
+// The kernel doesn't get to have syscalls, so it needs a shim
+static inline void* sbrk(intptr_t amt) {
+    void* oldHeap = heapEnd;
+    heapEnd += amt;
+    vMemMapInRange(oldHeap, heapEnd, VMemRead | VMemWrite);
+    if (heapEnd < (void*)KernelHeapStart || heapEnd > (void*)KernelHeapEnd) {
         panic(
-            "shim munmap of size %u at address 0x%X leaves heap into 0x%X\n"
-            "(current heap end is 0x%X)",
-            length, addr, end, heapEnd
+            "shim sbrk(%u) changed heap from 0x%X into 0x%X\n",
+            amt, oldHeap, heapEnd
         );
     }
-    int ret = vMemUnmapInRange(end, heapEnd);
-    if (ret < 0) {
-        panic("vMemUnmapInRange failed: %s", strerror(-ret));
-    }
-    return 0;
+    return oldHeap;
 }
 #endif // def __Kernel
 
@@ -173,153 +112,115 @@ void* malloc(size_t size) {
         return NULL;
     }
 
-    // Account for the order metadata
     size = memSizeToBlockSize(size);
-    size_t sizeOrder = lg(roundToPowerOf2(size));
-    if (sizeOrder < BuddyMinOrder) {
-        sizeOrder = BuddyMinOrder;
-    } else if (sizeOrder > BuddyMaxOrder) {
-        // Just mmap it, then...
-        size = pow2(sizeOrder);
-        AllocMemoryBlock* res = mmap(
-            NULL, size,
-            PROT_READ | PROT_WRITE, MAP_ANONYMOUS, -1, 0
-        );
-        if (res == MAP_FAILED) {
-            return NULL;
-        }
-        res->order = sizeOrder;
-        return (void*)res->data;
-    }
-
-    FreeMemoryBlock* block = freeLists[sizeOrder - BuddyMinOrder];
-    if (block == NULL) {
-        // Oops, no blocks available! Find a new one
+    MemoryBlock* block = NULL;
+    if (freeList != NULL) {
+        MemoryBlock* prevBlock = NULL;
+        MemoryBlock* curBlock = freeList;
         for (
-            size_t orderUp = sizeOrder + 1; orderUp <= BuddyMaxOrder; orderUp++
+            ;
+            curBlock != NULL;
+            prevBlock = curBlock, curBlock = curBlock->nextFree
         ) {
-            block = freeLists[orderUp - BuddyMinOrder];
-            if (block != NULL) {
-                // Awesome! Remove this from its free list
-                freeLists[orderUp - BuddyMinOrder] = block->next;
+            if (blockGetSize(curBlock) >= size) {
+                // Cool, this one fits!
+                if (prevBlock != NULL) {
+                    // Remove from linked list
+                    prevBlock->nextFree = curBlock->nextFree;
+                } else {
+                    // First item in list
+                    freeList = curBlock->nextFree;
+                }
+                block = curBlock;
                 block->isFree = 0;
                 break;
             }
         }
-
-        if (block == NULL) {
-            // Oops, there are no free blocks.
-            // That's okay, we can make some!
-            FreeMemoryBlock* newPool = mmap(
-                NULL, PoolSize, PROT_READ | PROT_WRITE, MAP_ANONYMOUS, -1, 0
-            );
-            if (newPool == MAP_FAILED) {
-                // oh goodness no we can't
-                return NULL;
-            }
-
-            for (size_t i = 0; i < BlocksInPool; i++) {
-                newPool->order = BuddyMaxOrder;
-                newPool->isFree = 1;
-                newPool->next = freeLists[BuddyMaxOrder - BuddyMinOrder];
-                freeLists[BuddyMaxOrder - BuddyMinOrder] = newPool;
-                newPool = (void*)newPool + pow2(BuddyMaxOrder);
-            }
-
-            // Now take a block from the free list
-            block = freeLists[BuddyMaxOrder - BuddyMinOrder];
-            freeLists[BuddyMaxOrder - BuddyMinOrder] = block->next;
-            block->isFree = 0;
-        }
-
-        // Cool, found one!
-        // Split this block up into sizeOrder blocks and allocate one
-        for (
-            size_t orderDown = block->order - 1;
-            orderDown >= sizeOrder;
-            orderDown--
-        ) {
-            // Split the block: we get the block and its buddy
-            FreeMemoryBlock* buddy = getBuddy(block, pow2(orderDown));
-            block->order = orderDown;
-            buddy->order = orderDown;
-            // The buddy becomes free
-            buddy->isFree = 1;
-            buddy->next = freeLists[orderDown - BuddyMinOrder];
-            freeLists[orderDown - BuddyMinOrder] = buddy;
-            // The block is further whitled down
-        }
-    } else {
-        // Cool, there are already free blocks!
-        // Dequeue the first one
-        block->isFree = 0;
-        freeLists[sizeOrder - BuddyMinOrder] = block->next;
     }
 
-    // Alright, now we have an appropriately sized block,
-    // fresh off the free list.
-    return ((AllocMemoryBlock*)block)->data;
+    if (block == NULL) {
+        // couldn't find a block big enough, make one
+        size_t poolSize = Max(memSizeToBlockSize(PoolSize), size);
+        block = sbrk(poolSize);
+        if (block == MAP_FAILED) {
+            // oh stars
+            return NULL;
+        }
+        memset(block, 0, sizeof(MemoryBlock));
+        blockSetSize(block, poolSize);
+        block->prevBlock = largestBlockAddress;
+        if (block->prevBlock) {
+            block->prevBlock->nextBlock = block;
+        }
+        largestBlockAddress = block;
+        if (block->prevBlock) assert(block->prevBlock->nextBlock == block);
+    }
+
+    // To drop blockSize when not needed
+    {
+        size_t blockSize = blockGetSize(block);
+        if (size < blockSize && blockSize - size >= MinBlockSize) {
+            // Split this block
+            blockSetSize(block, size);
+            MemoryBlock* nextBlock = (MemoryBlock*)((uintptr_t)block + size);
+            blockSetSize(nextBlock, blockSize - size);
+            nextBlock->isFree = 1;
+            nextBlock->prevBlock = block;
+            nextBlock->nextBlock = block->nextBlock;
+            block->nextBlock = nextBlock;
+            if (nextBlock->nextBlock != NULL) {
+                // Set the block after nextBlock to point back to
+                // nextBlock
+                nextBlock->nextBlock->prevBlock = nextBlock;
+            } else {
+                // This should be the block with the highest address
+                if (nextBlock > largestBlockAddress) {
+                    largestBlockAddress = nextBlock;
+                }
+            }
+            freeListAdd(nextBlock);
+        }
+    }
+
+    if (block->prevBlock) {
+        // TODO: there's a hole in this logic somewhere
+        // such that prevBlock->nextBlock isn't being set.
+        // This is a hackfix.
+        block->prevBlock->nextBlock = block;
+    }
+
+    // And there we have it
+    return block->data;
 }
 
-// Free is here too so it can have access to the kernel shims
+// Free is here too so it can have access to the kernel shim
 void free(void* addr) {
     if (addr == NULL) {
         return;
     }
 
-    AllocMemoryBlock* block = voidToMemoryBlock(addr);
-
-    if (block->order > BuddyMaxOrder) {
-        // This block was just mmap'd in, so just mmap it out
-        int ret = munmap(block, pow2(block->order));
-        return;
+    MemoryBlock* block = voidToMemoryBlock(addr);
+    assert(!block->isFree);
+    // Cool, here's our block!
+    // Check for coalescence
+    bool alreadyFree = false;
+    if (block->prevBlock != NULL && block->prevBlock->isFree) {
+        // Combine block into previous block
+        assert(block->prevBlock->nextBlock == block);
+        block->isFree = 1;
+        block = block->prevBlock;
+        blockMergeWithNext(block);
+        // The conditions for merging imply this block is on the free list
+        alreadyFree = true;
     }
 
-    block->isFree = 1;
-    for (
-        size_t orderUp = block->order + 1;
-        orderUp <= BuddyMaxOrder;
-        orderUp++
-    ) {
-        // A bit presumptuous to call it free, but the layouts for both blocks
-        // should align
-        FreeMemoryBlock* buddy = getBuddy(block, pow2(block->order));
-        if (!buddy->isFree) {
-            // No go, the buddy is allocated
-            break;
-        }
-        // Okay, we can keep coalescing
-        // This buddy is no longer free
-        buddy->isFree = 0;
-        // Set this block to have its new order
-        block->order = orderUp;
+    if (block->nextBlock != NULL && block->nextBlock->isFree) {
+        blockMergeWithNext(block);
+    }
 
-        // Now the buddy has been absorbed into the block
-        // The issue now is yanking the buddy from the free list
-        // TODO: locking
-        FreeMemoryBlock* prevList = NULL;
-        FreeMemoryBlock* freeList = freeLists[orderUp - 1 - BuddyMinOrder];
-        // Of course, if the buddy's free, this should hold:
-        assert(freeList != NULL);
-
-        // Unfortunately, we must loop over the free list.
-        for (
-            ;
-            freeList != NULL;
-            prevList = freeList, freeList = freeList->next
-        ) {
-            if (freeList == buddy) {
-                // Aha! There you are!
-                if (prevList != NULL) {
-                    // Remove the buddy from the free list
-                    prevList->next = freeList->next;
-                } else {
-                    // This must be the first one
-                    freeLists[orderUp - 1 - BuddyMinOrder] = freeList->next;
-                }
-                break;
-            }
-        }
+    if (!alreadyFree) {
+        block->isFree = 1;
+        freeListAdd(block);
     }
 }
 
@@ -335,13 +236,11 @@ void* realloc(void* ptr, size_t size) {
         return NULL;
     }
 
-    // Round size
-    size = roundToPowerOf2(memSizeToBlockSize(size));
-    AllocMemoryBlock* block = voidToMemoryBlock(ptr);
-    size_t blockSize = pow2(block->order);
+    MemoryBlock* block = voidToMemoryBlock(ptr);
+    size_t oldAllocSize = blockGetSize(block) - sizeof(MemoryBlock);
 
     // Determined by the new size or the current size, whichever is smaller
-    size_t bytesToPreserve = size < blockSize ? size : blockSize;
+    size_t bytesToPreserve = Min(size, oldAllocSize);
 
     // Not sure if shrinking the allocated data would be good
     // This may lead to smaller fragments being created than this method
