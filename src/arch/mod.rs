bc3301453be41166a819a24247283f90c65a09d8
Ruby Lord <nipkraps@gmail.com>
2020-06-05 12:21:32 -0500

    arch: use general without_interrupts function so all platforms benefit
    This replaces the use of x86_64's without_interrupts with a hand-spun
    function that depends on any architecture's given
    are_interrupts_enabled, enable_interrupts, and disable_interrupts
    functions. This is just one less thing anyone porting Spinel has
    to provide as part of the `arch` reexport interface :>

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -6,8 +6,21 @@ cfg_if::cfg_if! {
         pub use amd64::central::arch_info;
         pub use amd64::central::init::arch_init;
         pub use x86_64::instructions::interrupts::{
-            enable as enable_interrupts, disable as disable_interrupts,
-            without_interrupts
+            are_enabled as are_interrupts_enabled,
+            enable as enable_interrupts,
+            disable as disable_interrupts
         };
+    }
+}
+
+/// Run a function with interrupts disabled.
+/// Afterwards, this function restores interrupts to their
+/// previous state, whether they were enabled or disabled.
+pub fn without_interrupts<T>(f: impl FnOnce() -> T) {
+    let should_enable = are_interrupts_enabled();
+    disable_interrupts();
+    f();
+    if should_enable {
+        enable_interrupts();
     }
 }
