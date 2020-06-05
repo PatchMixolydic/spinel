cc4006ed62efb2d677f1f6a8dc3b5dd03face8eb
Ruby Lord <nipkraps@gmail.com>
2020-06-05 11:57:57 -0500

    arch: use cfg_if! to DRY up exports

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -1,7 +1,12 @@
 #[cfg(target_arch = "x86_64")]
 pub mod amd64;
 
-#[cfg(target_arch = "x86_64")]
-pub use amd64::central::arch_info;
-#[cfg(target_arch = "x86_64")]
-pub use amd64::central::init::arch_init;
+cfg_if::cfg_if! {
+    if #[cfg(target_arch = "x86_64")] {
+        pub use amd64::central::arch_info;
+        pub use amd64::central::init::arch_init;
+        pub use x86_64::instructions::interrupts::{
+            enable as enable_interrupts, disable as disable_interrupts
+        };
+    }
+}
